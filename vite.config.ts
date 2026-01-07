c5d2db697593978325da697589e69f5bcb4d19bf
Valentin Schroeter <valentin.schroeter@student.hpi.de>
2026-01-07 14:53:38 +0100

    Set base URL for GitHub Pages deployment in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/battery-lifetime-calculator/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
